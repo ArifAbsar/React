25a71ed64f16647b025a35fe179d432d24b9e9f1
Arif Absar <arifabsar182@gmail.com>
2024-06-06 16:05:17 +0600

    Fixed, will change the UI again

--- src/pages/Switch.jsx
+++ src/pages/Switch.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 
 const Switch = ({ isOn, handleToggle }) => {
   return (
-    <div className="flex items-center justify-center w-full py-4">
+    <div className="flex items-center justify-center w-full py-2">
+     <h2 className='fixed left-14'>Dark Mode</h2> 
       <div
-        className={`w-14 h-8 flex items-center bg-gray-300 rounded-full p-1 cursor-pointer ${
+        className={`w-10 h-6 flex items-center bg-gray-300 rounded-full p-1 cursor-pointer ${
           isOn ? 'bg-green-400' : ''
         }`}
         onClick={handleToggle}
       >
         <div
-          className={`bg-white w-6 h-6 rounded-full shadow-md transform ${
-            isOn ? 'translate-x-6' : ''
+          className={`bg-white w-4 h-4 rounded-full shadow-md transform ${
+            isOn ? 'translate-x-4' : ''
           }`}
         />
       </div>
