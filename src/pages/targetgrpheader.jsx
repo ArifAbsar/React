4f5f34141fec4b72e9b08a68449d0214c84b7339
21-44877-2 <21-44877-2@student.aiub.edu>
2024-06-24 15:33:51 +0600

    something

---
+++ src/pages/targetgrpheader.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Typography } from "@material-tailwind/react";
+
+const HeaderGRP = () => {
+  return (
+    <div className="my-0">
+      <Typography variant="h5" className="font-bold  text-gray-800 dark:text-gray-100 text-sm sm:text-base">
+        Markets
+      </Typography>
+      <div className="h-1 bg-gradient-to-r from-blue-500 to-purple-500 rounded-md w-32 mb-4"></div>
+    </div>
+  );
+};
+
+export default HeaderGRP;
